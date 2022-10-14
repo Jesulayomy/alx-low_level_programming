971e86f8deb004a5b616f533540f931d71e1089b
Jesulayomy <jesulayomi081@gmail.com>
2022-10-14 03:04:55 -0700

    Combination print

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - main code
+ *
+ * Return: 0 for successful
+ */
+int main(void)
+{
+	int numb;
+	int n = 0;
+
+	for (numb = 48; numb < 58; numb++)
+	{
+		putchar(numb);
+		if (numb == 57)
+		{
+			break;
+		}
+		putchar(44);
+		putchar(32);
+	}
+
+	putchar(10);
+
+	return (0);
+}
+
