348827ce2172e32f9e272d117d8e930a187fb1f4
Jesulayomy <jesulayomi081@gmail.com>
2022-10-18 11:45:56 -0700

    timess

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,32 +7,42 @@
  */
 void times_table(void)
 {
-	int r, c, n;
+	int r, c, n, t, u;
+
 	for (r = 0; r < 10; r++)
 	{
 		for (c = 0; c < 10; c++)
 		{
-			for (n = 0; n <= 9; n++)
+			n = c * r;
+
+			if (c > 0)
 			{
-				int d = (c * n)
-				
+				_putchar(44);
+				_putchar(32);
 
-
-
-
-
-
-				if (n < 9)
+				if (n < 10)
 				{
-					_putchar(44);
-					_putchar(32);
-				}
-				else
-				{
-					;
+					_putchar(' ');
 				}
 			}
-			_putchar(10);
+
+			if (n > 9)
+			{
+				t = n;
+
+				while (t >= 10)
+					t = t / 10;
+
+				u = n % 10;
+
+				_putchar(t + 48);
+				_putchar(u + 48);
+			}
+			else
+			{
+				_putchar(n + 48);
+			}
 		}
+		_putchar(10);
 	}
 }
