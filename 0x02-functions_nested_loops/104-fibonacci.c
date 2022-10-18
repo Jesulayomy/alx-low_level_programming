c51a73df6ca68b468b260c86788ede50a11a2185
Jesulayomy <jesulayomi081@gmail.com>
2022-10-18 15:50:06 -0700

    Last one

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -4,44 +4,58 @@
 void print_fibonacci(int);
 
 /**
- * main - sus fibonacci
+ * main - for callling
  *
- * Return: zero at success
+ * Return: ret 0
  */
 int main(void)
 {
-	int w = 98;
+	int n = 98;
 
-	print_fibonacci(w);
+	print_fibonacci(n);
 
 	return (0);
 }
 
 /**
- * print_fibonacci - prints out the fibonacci sequence of numbers
- * @i: the input number
- *
- * Return: returns nothing
+ * print_fibonacci - the sequence
+ * @i: input
  */
 void print_fibonacci(int i)
 {
 	int a;
-	long int b;
+	long int b, fb, lb, fl, ll;
 	long int l = 2;
 	long int p = 1;
 
 	printf("%ld, %ld, ", p, l);
 
-	for (a = 3; a <= i; a++)
+	for (a = 3; a < 89; a++)
 	{
 		b = p + l;
 
-		printf("%ld", b);
-		if (a != i)
-			printf(", ");
+		printf("%ld, ", b);
 
 		p = l;
 		l = b;
 	}
-	printf("\n");
+
+	fl = l / 1000000000;
+	ll = l % 1000000000;
+	fb = b / 1000000000;
+	lb = b % 1000000000;
+
+	for (a = 89; a < i; a++)
+	{
+		printf("%ld%ld, ", fb, lb);
+
+		p = fl;
+		l = ll;
+		fl = fb;
+		ll = lb;
+		fb = p + fl + ((l + ll) / 1000000000);
+		lb = (l + ll) % 1000000000;
+	}
+
+	printf("%ld%ld\n", fb, lb);
 }
