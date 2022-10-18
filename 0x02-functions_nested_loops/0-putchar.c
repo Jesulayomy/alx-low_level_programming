41be595a60ce2d6c6ea22f2edc10779c28f56931
Jesulayomy <jesulayomi081@gmail.com>
2022-10-17 23:07:04 -0700

    A new type of function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+/**
+ * main - This program uses only putchar with ascii values
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+	_putchar(95);
+	_putchar(112);
+	_putchar(117);
+	_putchar(116);
+	_putchar(99);
+	_putchar(104);
+	_putchar(97);
+	_putchar(114);
+	_putchar(10);
+
+	return (0);
+}
