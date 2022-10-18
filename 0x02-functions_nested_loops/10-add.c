f14012430a443f69658e5c9779e6892a99c4953c
Jesulayomy <jesulayomi081@gmail.com>
2022-10-18 08:15:55 -0700

    skipped 9, hers's 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers
+ *
+ * Return: nothing
+ */
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return(sum);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * times_table - back to basic 1
+ *
+ * Return: none
+ */
+void times_table(void)
+{
+	int r, c, n;
+	for (r = 0; r < 10; r++)
+	{
+		for (c = 0; c < 10; c++)
+		{
+			for (n = 0; n <= 9; n++)
+			{
+				int d = (c * n)
+				
+
+
+
+
+
+
+				if (n < 9)
+				{
+					_putchar(44);
+					_putchar(32);
+				}
+				else
+				{
+					;
+				}
+			}
+			_putchar(10);
+		}
+	}
+}
