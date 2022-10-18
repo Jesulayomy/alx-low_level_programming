03a874547f99785961e89da6e18107a9ffeefd1f
Jesulayomy <jesulayomi081@gmail.com>
2022-10-18 15:08:03 -0700

    updates to mainhead

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,15 +9,15 @@
  */
 void print_times_table(int n)
 {
-	int i, j, k;
+	int l, j, k;
 
 	if (n >= 0 && n <= 15)
 	{
-		for (i = 0; i <= n; i++)
+		for (l = 0; l <= n; l++)
 		{
 		for (j = 0; j <= n; j++)
 		{
-			k = j * i;
+			k = j * l;
 			if (j != 0)
 			{
 				printf(", ");
