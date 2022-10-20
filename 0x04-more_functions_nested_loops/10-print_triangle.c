0c34da582f0882d2e0bb85bc98310242886ac024
Jesulayomy <jesulayomi081@gmail.com>
2022-10-20 05:14:23 -0700

    fix 3

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle using #
+ * @size: - the size of the square. 0 or less means no quare, only newline
+ */
+void print_triangle(int size)
+{
+	int r, c;
+
+	if (size <= 0)
+	{
+		_putchar(10);
+	}
+	else
+	{
+		for (r = size; r > 0; r--)
+		{
+			for (c = size; c > 0; c--)
+			{
+				_putchar(35);
+			}
+			_putchar(10);
+		}
+	}
+}
--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,6 +6,7 @@
 void print_numbers(void)
 {
 	int n = 0;
+
 	while (n < 10)
 	{
 		_putchar(n + 48);
