08ea2616966a29759b630a61caa8cbd594b67a31
Jesulayomy <jesulayomi081@gmail.com>
2022-10-20 01:12:24 -0700

    The three that came first

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - finds the product of two digits
+ * @a: - d1
+ * @b: - d2
+ *
+ * Return: the value of the product
+ */
+int mul(int a, int b)
+{
+	int product = a * b;
+
+	return (product);
+}
