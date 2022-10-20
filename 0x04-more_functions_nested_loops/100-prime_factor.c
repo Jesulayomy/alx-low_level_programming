9ab1289339f80e36b38f7f7b9522b30da7bfae38
Jesulayomy <jesulayomi081@gmail.com>
2022-10-20 07:37:39 -0700

    Super Prime

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - prime number
+ * Return: 0 
+ */
+int main(void)
+{
+	int i;
+
+	long int n = 612852475143;
+	long int org = n;
+
+	while (n % 2 == 0)
+	{
+		n = n / 2;
+	}
+
+	for (i = 3; i <= sqrt(org); i = i + 2)
+	{
+		while ((n % i) == 0)
+		{
+			n = n / i;
+		}
+	}
+	printf("%ld\n", n);
+
+	return (0);
+}
