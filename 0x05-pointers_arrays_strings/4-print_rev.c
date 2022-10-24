39d1f1c9ec50c1a4433d449f4298e0839459a6b5
Jesulayomy <jesulayomi081@gmail.com>
2022-10-24 15:19:41 +0000

    Task 4contd

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - a fake inverse string printer
+ * @s: the pointer to a string
+ *
+ * Return: nil
+ */
+void print_rev(char *s)
+{
+	int i, s, n
+
+	/* Count the number of elements in the array */
+	for (i = 0; str[i]; i++)
+	{
+		;
+	}
+
+	for (; 
+		s = (int) str[i];
+		_putchar(s);
+	}
+	_putchar(10);
+}
