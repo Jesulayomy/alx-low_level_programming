530f985b4eea85a2ebac31795d219f3a502aaf8c
Jesulayomy <jesulayomi081@gmail.com>
2022-10-24 01:48:33 -0700

    Working on 1-swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps two integers lol
+ * @a: numero uno
+ * @b: numero dos
+ *
+ * Return: non
+ */
+void swap_int(int *a, int *b)
+{
+	int *swapa = a;
+	int *swapb = b;
+	
+}
