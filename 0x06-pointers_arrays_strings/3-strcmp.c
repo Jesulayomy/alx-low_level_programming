7e40bf701d75a5d626014ed02c158f70d2a2a36c
Jesulayomy <jesulayomi081@gmail.com>
2022-10-27 05:15:59 -0700

    fixes fixes

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,15 +12,22 @@ int _strcmp(char *s1, char *s2)
 {
 	int cs1, cs2;
 
-	for (cs1 = 0; s1[cs1]; cs1++)
+	for (cs1 = 0; s1[cs1] != '\0'; cs1++)
 	{
 		;
 	}
 
-	for (cs2 = 0; s2[cs2]; cs2++)
+	for (cs2 = 0; s2[cs2] != '\0'; cs2++)
 	{
 		;
 	}
 
-	return (cs1 - cs2);
+	if (cs1 == cs2)
+	{
+		return (0);
+	}
+	else
+	{
+		return (s1 - s2);
+	}
 }
