fdcc791a6747a7ecdb6e69a239caf0bb2b34a9e1
Jesulayomy <jesulayomi081@gmail.com>
2022-10-31 19:10:15 +0000

    Take two

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <math.h>
+#include <time.h>
+
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+
+
+
+#endif
