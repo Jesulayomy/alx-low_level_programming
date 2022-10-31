95e39e4fdae6aac3e2edb071e1804e6df22a6a26
Jesulayomy <jesulayomi081@gmail.com>
2022-10-31 20:29:38 +0000

    update 7

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,6 +8,7 @@
 #include <time.h>
 
 char *_memset(char *s, char b, unsigned int n);
+int _putchar(char c);
 void print_chessboard(char (*a)[8]);
 void set_string(char **s, char *to);
 void print_diagsums(int *a, int size);
