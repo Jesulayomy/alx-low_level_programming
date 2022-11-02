138fb39cdfa5f13557892ba089b80f90880f0b9f
Jesulayomy <jesulayomi081@gmail.com>
2022-11-02 13:08:05 +0000

    auto commit

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,27 @@
 #include "main.h"
 
 /**
+ * is_prime - checks for prime
+ * @num: the no to check
+ * @count: a counter starting from 2 upwards
+ *
+ * Return: 1 if prime, 0 if not
+ */
+int is_prime(int num, int count)
+{
+	if (num == count)
+	{
+		return (1);
+	}
+	else if ((num % count) == 0)
+	{
+		return (0);
+	}
+
+	return (is_prime(num, count + 1));
+}
+
+/**
  * is_prime_number - checks for prime
  * @n: the no to check
  *
@@ -8,8 +29,10 @@
  */
 int is_prime_number(int n)
 {
+	if (n <= 1)
+	{
+		return (0);
+	}
 
-
-
-
+	return (is_prime(n, 2));
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,6 +7,8 @@
 #include <unistd.h>
 
 void _puts_recursion(char *s);
+int is_prime(int, int);
+int is_prime_number(int n);
 void _print_rev_recursion(char *s);
 int _putchar(char c);
 int _strlen_recursion(char *s);
