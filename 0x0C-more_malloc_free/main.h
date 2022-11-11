62fda0d7ffcc052c49a0322e98460aea2bfef67e
Jesulayomy <jesulayomi081@gmail.com>
2022-11-11 00:54:58 +0000

    mul

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include <limits.h>
 
 void *malloc_checked(unsigned int);
@@ -12,5 +13,8 @@ int _putchar(char);
 void *_calloc(unsigned int, unsigned int);
 int *array_range(int, int);
 void *_realloc(void *, unsigned int, unsigned int);
+void _is_zero(char *argv[]);
+char *_initialize_array(char *, int);
+int _checknum(char *argv[], int);
 
 #endif
