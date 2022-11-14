1f3779016c71ddef9547e7c140243f7182699d34
Jesulayomy <jesulayomi081@gmail.com>
2022-11-14 12:21:38 +0000

    All

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print compile file name
+ *
+ * Return: 0 at successful exit
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef ABS_H_
+#define ABS_H_
+
+#define ABS(X) ((X) < 0 ? (-X) : (X))
+
+#endif
