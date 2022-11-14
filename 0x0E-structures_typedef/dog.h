8c1b498a2d6fea69f9cdac772c11723ff0fcb46e
Jesulayomy <jesulayomi081@gmail.com>
2022-11-14 13:08:11 +0000

    doggo1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOGH_
+#define DOGH_
+
+/**
+ * struct dog - creates a dogtag
+ * @name: name of doggo
+ * @age: age of doggo
+ * @owner: of doggo
+ *
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif
