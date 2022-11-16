a620d915ad4a16bfe22dcf46ece4a8a6a7d76924
Jesulayomy <jesulayomi081@gmail.com>
2022-11-16 12:05:34 +0000

    Update main

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,29 +10,31 @@
 int main(int argc, char *argv[])
 {
 	int (*r)(int, int);
-	int n;
+	int a1, a3, n;
+	char a2;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit (98);
 	}
-
-	r = get_op_func((argv[2]));
-	if (r == -1)
+	r = get_op_func(argv[2]);
+	if (!r)
 	{
 		printf("Error\n");
 		exit(99);
 	}
+	a1 = atoi(argv[1]);
+	a2 = *argv[2];
+	a3 = atoi(argv[3]);
 
-	n = r(atoi(argv[1]), atoi(argv[2]));
-	if (n == NULL)
+	if ((a2 == 47 || a2 == 37) && a3 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
+	n = r(a1, a3);
 	printf("%d\n", n);
-
 	return (0);
 }
