d42ded99c026215736cb4c77ecc00307b789669c
Jesulayomy <jesulayomi081@gmail.com>
2022-11-16 07:34:17 +0000

    Arr loop func

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes the specified array
+ * @array: to execute
+ * @size: of the array
+ * @action: function to be executed
+ *
+ * Returns: non
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+
+}
