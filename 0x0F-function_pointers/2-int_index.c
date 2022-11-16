3b344428aa31fe3b45bc303391a82be2dcaa978a
Jesulayomy <jesulayomi081@gmail.com>
2022-11-16 09:26:55 +0000

    search

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an int
+ * @array: of ints to index
+ * @size: of elements in the array
+ * @cmp: function to compare values
+ *
+ * Return: index to the first element where cmp returns !0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int not0, i;
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		not0 = cmp(array[i]);
+
+		if (not0 != 0)
+			return (i);
+	}
+
+	return (-1);
+}
