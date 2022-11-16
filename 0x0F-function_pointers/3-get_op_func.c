af4dd0371576ea871783d1a3283e13ad98ab9de7
Jesulayomy <jesulayomi081@gmail.com>
2022-11-16 11:39:19 +0000

    fix

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -8,6 +8,7 @@
  */
 int (*get_op_func(char *s))(int a, int b)
 {
+	int (*ptr_to_op)(int, int);
 	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
@@ -19,12 +20,15 @@ int (*get_op_func(char *s))(int a, int b)
 	int i;
 
 	i = 0;
-	while (ops[i])
+	while (i < 5)
 	{
-		if ((op[i][0]) == (char *s))
-			return (op[i][1](a, b));
+		if (s == ops[i].op)
+		{
+			ptr_to_op = ops[i].f;
+			return (ptr_to_op);
+		}
 		i++;
 	}
 
-	return (NULL);
+	return (-1);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,4 +1,4 @@
-#include "3-calc.c"
+#include "3-calc.h"
 
 /**
  * main - calls the func
@@ -9,19 +9,30 @@
  */
 int main(int argc, char *argv[])
 {
+	int (*r)(int, int);
+	int n;
+
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit (98);
 	}
 
-	
+	r = get_op_func((argv[2]));
+	if (r == -1)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
+	n = r(atoi(argv[1]), atoi(argv[2]));
+	if (n == NULL)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
+	printf("%d\n", n);
 
-
-
-
-
-
+	return (0);
 }
