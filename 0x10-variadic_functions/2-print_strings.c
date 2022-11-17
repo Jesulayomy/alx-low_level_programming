10a8860b85ef949a51dc7948f0efe621876b552b
Jesulayomy <jesulayomi081@gmail.com>
2022-11-17 16:15:43 +0000

    varg

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -27,9 +27,11 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		else
 			printf("%s", str);
 
-		if (separator != NULL && i != n - 1)
+		if (separator && i != n - 1)
 			printf("%s", separator);
 	}
+
+	va_end(args);
 
 	printf("\n");
 }
