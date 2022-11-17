0ae24d85483001dd498e62bca2b45fb1b2019252
Jesulayomy <jesulayomi081@gmail.com>
2022-11-17 15:48:25 +0000

    varprints

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers in order
+ * @separator: the char to add between them
+ * @n: the number of arguments to print
+ *
+ * Return: nothing, its void bozo
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	int num;
+	va_list args;
+
+	if (n == 0)
+		return;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		num = va_arg(args, int);
+		printf("%d", num);
+
+		if (separator != NULL && (i < n - 1))
+			printf("%s", separator);
+	}
+
+	printf("\n");
+}
