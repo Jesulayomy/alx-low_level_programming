986122be07a3cdcfa4e5cc5a2dec331a878b4d8f
Jesulayomy <jesulayomi081@gmail.com>
2022-11-24 14:54:06 +0000

    100

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -24,6 +24,7 @@ list_t *add_node(list_t **, const char *);
 list_t *add_node_end(list_t **, const char *);
 void free_list(list_t *);
 size_t list_len(const list_t *);
+void first(void) __attribute__ ((constructor));
 size_t print_list(const list_t *);
 
 #endif
