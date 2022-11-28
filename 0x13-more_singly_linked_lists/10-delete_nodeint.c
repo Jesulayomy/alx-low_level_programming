51a05c6ea36a8060854ea7da3c673d4b56766f61
Jesulayomy <jesulayomi081@gmail.com>
2022-11-28 18:15:31 +0000

    betty

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -21,7 +21,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 
-	for (; count < index - 1; count ++)
+	for (; count < index - 1; count++)
 	{
 		if (!temp->next)
 			return (-1);
