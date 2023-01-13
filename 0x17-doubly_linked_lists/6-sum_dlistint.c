5e067af0d01cb583c3c3bfd51c7aa11f51124e15
Jesulayomy <jesulayomi081@gmail.com>
2023-01-13 02:53:40 +0000

    sum of terms

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - returns the sum of all the data in a dlistint_t list
+ * @head: pointer to head
+ *
+ * Return: sum
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp = head;
+	int n = 0;
+
+	if (head == NULL)
+		return (0);
+
+	while (temp)
+	{
+		n += temp->n
+		temp = temp->next;
+	}
+
+	return (n);
+}
