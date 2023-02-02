723dd070ffafece0a8ff2cfa6cac38b8bdc2c5d5
Jesulayomy <jesulayomi081@gmail.com>
2023-02-02 12:13:38 -0800

    hello

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the table to create
+ *
+ * Return: a pointer to the created table or null
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hasht;
+	unsigned long int i;
+
+	if (size == 0)
+		return (NULL);
+	
+	hasht = malloc(sizeof(hash_table_t));
+	if (hasht == NULL)
+		return (NULL);
+
+	hasht->size = size;
+	hasht->array = malloc(sizeof(hash_node_t *) * size);
+	if (hasht->array == NULL)
+	{
+		free(hasht);
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+		hasht->array[i] = NULL;
+
+	return (hasht);
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,3 @@
+<h1 align='center'> Hash Tables </h1>
+
+This Repo covers the basic to advanced concepts covered in Alx's software engineerng course
