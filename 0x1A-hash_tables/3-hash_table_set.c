73c60a9a9ac911c8dd20d32c635d11a56f4d040b
Jesulayomy <jesulayomi081@gmail.com>
2023-02-02 19:19:39 -0800

    test

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,14 +11,13 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int idx;
-	hash_node_t *new;
+	hash_node_t *new, *temp;
 
-	if (key[0] == '\0' || key == NULL)
-		return (0);
-	if (value == NULL)
+	if (value == NULL || key[0] == '\0' || key == NULL || ht == NULL)
 		return (0);
 
 	idx = key_index((unsigned char *)key, ht->size);
+	temp = ht->array[idx];
 	if (ht->array[idx] == NULL)
 	{
 		ht->array[idx] = malloc(sizeof(value) + 1);
@@ -28,12 +27,22 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	else
 	{
+		while (temp)
+		{
+			if (strcmp(temp->key, key) == 0)
+			{
+				free(temp->value);
+				temp->value = strdup(value);
+				return (1);
+			}
+			temp = temp->next;
+		}
+		printf("makind new node\n");
 		new = malloc(sizeof(value) + 1);
 		new->key = strdup(key);
 		new->value = strdup(value);
 		new->next = ht->array[idx];
 		ht->array[idx] = new;
 	}
-
 	return (1);
 }
--- 0x1A-hash_tables/tests/3-main.c
+++ 0x1A-hash_tables/tests/3-main.c
@@ -10,10 +10,8 @@ int main(void)
 	hash_table_t *ht;
 	unsigned long int idx;
 	unsigned long int idx2;
-	unsigned long int idx3;
 	char *key = "betty";
 	char *key2 = "aftty";
-	char *key3 = "besuy";
 
 	ht = hash_table_create(1024);
 	if (ht != NULL)
@@ -21,7 +19,6 @@ int main(void)
 
 	idx = key_index((unsigned char *)key, 1024);
 	idx2 = key_index((unsigned char *)key2, 1024);
-	idx3 = key_index((unsigned char *)key3, 1024);
 
 	hash_table_set(ht, key, "cool");
 	hash_table_set(ht, key2, "cool2");
@@ -29,11 +26,10 @@ int main(void)
 	printf("Key: %s\nValue: %s\n", ht->array[idx]->key, ht->array[idx]->value);
 	printf("Key: %s\nValue: %s\n", ht->array[idx2]->key, ht->array[idx2]->value);
 
-	hash_table_set(ht, key3, "cool3");
+	hash_table_set(ht, key2, "cool3");
 
 	printf("Key: %s\nValue: %s\n", ht->array[idx]->key, ht->array[idx]->value);
 	printf("Key: %s\nValue: %s\n", ht->array[idx2]->key, ht->array[idx2]->value);
-	printf("Key: %s\nValue: %s\n", ht->array[idx3]->next->key, ht->array[idx3]->next->value);
 
 	return (EXIT_SUCCESS);
 }
