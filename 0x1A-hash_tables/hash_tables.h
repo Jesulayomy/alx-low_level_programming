4c0f2e007e4206a6b28b85be04d83866418b87b3
Jesulayomy <jesulayomi081@gmail.com>
2023-02-03 17:33:47 -0800

    sorted hashes

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -74,18 +74,20 @@ typedef struct hash_table_s
 } hash_table_t;
 
 char *hash_table_get(const hash_table_t *, const char *);
+char *shash_table_get(const shash_table_t *ht, const char *key);
+hash_table_t *hash_table_create(unsigned long int);
 int hash_table_set(hash_table_t *, const char *, const char *);
 int shash_table_set(shash_table_t *ht, const char *key, const char *value);
-char *shash_table_get(const shash_table_t *ht, const char *key);
-void shash_table_print(const shash_table_t *ht);
-void shash_table_print_rev(const shash_table_t *ht);
-void shash_table_delete(shash_table_t *ht);
-hash_table_t *hash_table_create(unsigned long int);
 shash_table_t *shash_table_create(unsigned long int size);
+shash_node_t *add_n_shash(shash_node_t **h, const char *key, const char *value);
 unsigned long int hash_djb2(const unsigned char *);
 unsigned long int key_index(const unsigned char *, unsigned long int);
-void hash_table_print(const hash_table_t *);
-void hash_table_delete(hash_table_t *);
+void add_i_shash(shash_table_t *ht, shash_node_t *);
 void free_list(hash_node_t *);
+void hash_table_delete(hash_table_t *);
+void hash_table_print(const hash_table_t *);
+void shash_table_delete(shash_table_t *ht);
+void shash_table_print(const shash_table_t *ht);
+void shash_table_print_rev(const shash_table_t *ht);
 
 #endif
